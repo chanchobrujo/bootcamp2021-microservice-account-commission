335471327d1138f85db1855bd8c59500aac40918
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-09-24 16:19:01 -0500

    Init project.

---
+++ src/main/java/com/everisbootcamp/accountcommission/AccountCommissionApplication.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.accountcommission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountCommissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everisbootcamp/accountcommission/AccountCommissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.accountcommission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountCommissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
