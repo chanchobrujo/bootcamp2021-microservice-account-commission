24f31452ea3078da4d1dc9c8d08ee2070642d979
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-09-24 18:58:54 -0500

    Init project.

---
+++ src/main/java/com/everisbootcamp/accountcommission/Start/InitCommissión.java
@@ -0,0 +1,17 @@
+package com.everisbootcamp.accountcommission.Start;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class InitCommissión implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {  
+        log.info("GAAAAAAAAA");
+    }
+    
+}
